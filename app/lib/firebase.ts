8254066ac16628b583dfb970de419dfed80fed34
Lucas Heber <lucas.heber07@gmail.com>
2025-04-14 15:57:23 -0300

    feat(auth): implement Firebase authentication setup with Firestore adapter

---
+++ app/lib/firebase.ts
@@ -0,0 +1,24 @@
+import { cert, getApps, initializeApp } from 'firebase-admin/app';
+import { getFirestore } from 'firebase-admin/firestore';
+import 'server-only';
+
+const decodeKey = Buffer.from(
+    process.env.FIREBASE_PRIVATE_KEY_BASE64 || '',
+    'base64'
+).toString('utf-8');
+
+export const firebaseCert = cert({
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+    privateKey: decodeKey,
+});
+
+
+// Initialize Firebase Admin
+if (!getApps().length) {
+    initializeApp({
+        credential: firebaseCert,
+    });
+}
+
+export const db = getFirestore();
